8890feb7f26dc21a5dafa5ae4f58c3e1efd62f33
Himanshu Panwar <himanshupanwar001@gmail.com>
2018-05-24 01:33:31 +0530

    Add project files.

---
+++ MVC5InOneWeek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5InOneWeek.MvcApplication" Language="C#" %>
---
+++ MVC5InOneWeek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5InOneWeek.Startup))]
+namespace MVC5InOneWeek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
