d22152c724ffe4fedb92ca090eef42a7fc9fd951
Himanshu Panwar <himanshupanwar001@gmail.com>
2018-05-26 18:34:55 +0530

    Changes for the Day 2 Coding Session. Also added Project Notes files.

---
+++ MVC5InOneWeek/Controllers/TestController.cs
@@ -0,0 +1,47 @@
+﻿using MVC5InOneWeek.Models;
+using MVC5InOneWeek.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5InOneWeek.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View("MyView");
+        }
+        public string GetString()
+        {
+            return "Hello World is old now";
+        }
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+
+            EmployeeViewModel vmEmp = new EmployeeViewModel();
+            vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+            vmEmp.Salary = emp.Salary.ToString("C");
+            if (emp.Salary > 15000)
+            {
+                vmEmp.SalaryColor = "yellow";
+            }
+            else
+            {
+                vmEmp.SalaryColor = "green";
+            }
+
+            vmEmp.UserName = "Admin";
+
+            ViewBag.Employee = emp;
+            return View("MyView", vmEmp);
+        }
+    }
+}
---
+++ MVC5InOneWeek/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5InOneWeek.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+    }
+
+}
